650551903bdf92729cd40a7cf30623c75735c26f
Ken <kenbuslonkinoshita@gmail.com>
2023-07-11 00:51:11 +0900

    style: added style in search results

--- src/pages/homepage/RecipeDetailsModal.jsx
+++ src/pages/homepage/RecipeDetailsModal.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Modal } from "react-bootstrap";
+import { Modal, Row, Col } from "react-bootstrap";
 
 import HomepageUtils from "./utils/homepageUtils";
 
@@ -12,6 +12,7 @@ function RecipeDetailsModal(props) {
 			size="lg"
 			aria-labelledby="contained-modal-title-vcenter"
 			centered
+			dialogClassName="wideModal"
 		>
 			<Modal.Header closeButton>
 				<Modal.Title id="contained-modal-title-vcenter">
@@ -19,53 +20,58 @@ function RecipeDetailsModal(props) {
 				</Modal.Title>
 			</Modal.Header>
 			<Modal.Body>
-				{selectedRecipe.description ? (
-					<>
-						<h3 className="title is-6">Description</h3>
-						<h5 className="subtitle is-5">{selectedRecipe.description}</h5>
-					</>
-				) : (
-					""
-				)}
-
-				<div className="table-container">
-					<table className="table is-striped">
-						<thead>
-							<th>Ingredients</th>
-						</thead>
-						<tbody>
-							{selectedRecipe &&
-								selectedRecipe.sections.map((section) => {
-									return (
-										<>
-											{section.components.map((ingredient) => {
-												return (
-													<>
-														<tr>
-															<td>{ingredient.raw_text}</td>
-														</tr>
-													</>
-												);
-											})}
-										</>
-									);
-								})}
-						</tbody>
-					</table>
-					<h3 className="title is-6">Instruction</h3>
-
-					{selectedRecipe.instructions &&
-						selectedRecipe.instructions.map((instruction, index) => {
-							return (
-								<>
-									<p className="is-6">
-										{index + 1 + ". "}
-										{instruction.display_text}
-									</p>
-								</>
-							);
-						})}
-				</div>
+				<Row>
+					<Col lg={8} md={4}>
+						{selectedRecipe.description ? (
+							<>
+								<h3 className="title is-6">Description</h3>
+								<h5 className="is-5">{selectedRecipe.description}</h5>
+							</>
+						) : (
+							""
+						)}
+						<br></br>
+						<h3 className="title is-6">Instructions</h3>
+						{selectedRecipe.instructions &&
+							selectedRecipe.instructions.map((instruction, index) => {
+								return (
+									<>
+										<p className="is-6">
+											{index + 1 + ". "}
+											{instruction.display_text}
+										</p>
+									</>
+								);
+							})}
+					</Col>
+					<Col lg={4} md={4}>
+						<div className="table-container">
+							<table className="table is-striped is-narrow">
+								<thead>
+									<th>Ingredients</th>
+								</thead>
+								<tbody>
+									{selectedRecipe &&
+										selectedRecipe.sections.map((section) => {
+											return (
+												<>
+													{section.components.map((ingredient) => {
+														return (
+															<>
+																<tr>
+																	<td>{ingredient.raw_text}</td>
+																</tr>
+															</>
+														);
+													})}
+												</>
+											);
+										})}
+								</tbody>
+							</table>
+						</div>
+					</Col>
+				</Row>
 			</Modal.Body>
 			<Modal.Footer>
 				<button
--- src/pages/userRecipe/RecipeDetailsModal.jsx
+++ src/pages/userRecipe/RecipeDetailsModal.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Modal, Container, Row, Col } from "react-bootstrap";
+import { Modal, Row, Col } from "react-bootstrap";
 
 function RecipeDetailsModal(props) {
 	const { selectedRecipe, setEditRecipeShow } = props;
@@ -28,10 +28,9 @@ function RecipeDetailsModal(props) {
 				</Modal.Title>
 			</Modal.Header>
 			<Modal.Body>
-				<Container>
-					<Row>
-						<Col lg={4} md={4}>
-							{/* <figure className="image is-5by4">
+				<Row>
+					<Col lg={4} md={4}>
+						{/* <figure className="image is-5by4">
 								<img
 									src={
 										selectedRecipe.image_url
@@ -41,41 +40,40 @@ function RecipeDetailsModal(props) {
 									alt="Food Image"
 								/>
 							</figure> */}
-						</Col>
-						<Col lg={8} md={4}></Col>
-					</Row>
-					<Row>
-						<Col lg={8} md={4}>
-							<h3 className="title is-6">Description</h3>
-							<h5 className="subtitle is-5">{selectedRecipe.description}</h5>
-							<hr></hr>
-							<h3 className="title is-6">Instructions</h3>
-							<h5 className="subtitle is-5">{selectedRecipe.instruction}</h5>
-						</Col>
-						<Col lg={4} md={4}>
-							<div className="table-container">
-								<table className="table is-striped is-narrow">
-									<thead>
-										{ingredients.length < 1 ? "" : <th>Ingredients</th>}
-									</thead>
-									<tbody>
-										{ingredients.map((ingredient) => {
-											return (
-												<>
-													<tr>
-														<td>{ingredient.ingredient_info}</td>
-														{/* <td>{ingredient.ingredient_name}</td>
+					</Col>
+					<Col lg={8} md={4}></Col>
+				</Row>
+				<Row>
+					<Col lg={8} md={4}>
+						<h3 className="title is-6">Description</h3>
+						<h5 className="subtitle is-5">{selectedRecipe.description}</h5>
+						<hr></hr>
+						<h3 className="title is-6">Instructions</h3>
+						<h5 className="subtitle is-5">{selectedRecipe.instruction}</h5>
+					</Col>
+					<Col lg={4} md={4}>
+						<div className="table-container">
+							<table className="table is-striped is-narrow">
+								<thead>
+									{ingredients.length < 1 ? "" : <th>Ingredients</th>}
+								</thead>
+								<tbody>
+									{ingredients.map((ingredient) => {
+										return (
+											<>
+												<tr>
+													<td>{ingredient.ingredient_info}</td>
+													{/* <td>{ingredient.ingredient_name}</td>
 											<td>{ingredient.amount}</td> */}
-													</tr>
-												</>
-											);
-										})}
-									</tbody>
-								</table>
-							</div>
-						</Col>
-					</Row>
-				</Container>
+												</tr>
+											</>
+										);
+									})}
+								</tbody>
+							</table>
+						</div>
+					</Col>
+				</Row>
 			</Modal.Body>
 			<Modal.Footer>
 				<button
