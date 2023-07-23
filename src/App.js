49b7a651c2a9a3d0ba8bf9412697bd6f0daea926
Ken Kinoshita <89846582+iAmKenKinoshita@users.noreply.github.com>
2023-07-24 01:20:28 +0900

    chore: added new file for categories and populated navbar

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 //Pages
-import Navbar from "./pages/NavBar";
+import Navbar from "./pages/NavBar/NavBar";
 import CatBar from "./pages/CatBar";
 import Footer from "./pages/Footer";
 import Homepage from "./pages/homepage/Homepage";
---
+++ src/pages/NavBar/categories.js
@@ -0,0 +1,167 @@
+const categories = {
+	MEALS: [
+		{
+			name: "breakfast",
+			display_name: "Breakfast",
+		},
+		{
+			name: "brunch",
+			display_name: "Brunch",
+		},
+		{
+			name: "lunch",
+			display_name: "Lunch",
+		},
+		{
+			name: "dinner",
+			display_name: "Dinner",
+		},
+		{
+			name: "snack",
+			display_name: "Snack",
+		},
+		{
+			name: "sides",
+			display_name: "Sides",
+		},
+		{
+			name: "appetizers",
+			display_name: "Appetizers",
+		},
+		{
+			name: "soups",
+			display_name: "Soups",
+		},
+	],
+	CUISINE: [
+		{
+			name: "japanese",
+			display_name: "Japanese",
+		},
+		{
+			name: "french",
+			display_name: "French",
+		},
+		{
+			name: "korean",
+			display_name: "Korean",
+		},
+		{
+			name: "indian",
+			display_name: "Indian",
+		},
+		{
+			name: "mexican",
+			display_name: "Mexican",
+		},
+		{
+			name: "chinese",
+			display_name: "Chinese",
+		},
+		{
+			name: "italian",
+			display_name: "Italian",
+		},
+		{
+			name: "filipino",
+			display_name: "Filipino",
+		},
+		{
+			name: "north_american",
+			display_name: "American",
+		},
+	],
+	DIET: [
+		{
+			name: "gluten_free",
+			display_name: "Gluten Free",
+		},
+		{
+			name: "vegetarian",
+			display_name: "Vegetarian",
+		},
+		{
+			name: "keto",
+			display_name: "Keto",
+		},
+		{
+			name: "Vegan",
+			display_name: "Vegan",
+		},
+		{
+			name: "dairy_free",
+			display_name: "Dairy Free",
+		},
+		{
+			name: "pescatarian",
+			display_name: "Pescatarian",
+		},
+	],
+	METHOD: [
+		{
+			name: "baking",
+			display_name: "Baking",
+		},
+		{
+			name: "grill",
+			display_name: "Grill",
+		},
+		{
+			name: "deep_fry",
+			display_name: "Deep Fry",
+		},
+
+		{
+			name: "steam",
+			display_name: "Steam",
+		},
+		{
+			name: "stuffed",
+			display_name: "Stuffed",
+		},
+		{
+			name: "pan_fry",
+			display_name: "Pan Fry",
+		},
+	],
+	SEASONAL: [
+		{
+			name: "fall",
+			display_name: "Fall",
+		},
+		{
+			name: "summer",
+			display_name: "Summer",
+		},
+		{
+			name: "winter",
+			display_name: "Winter",
+		},
+		{
+			name: "spring",
+			display_name: "Spring",
+		},
+		{
+			name: "christmas",
+			display_name: "Christmas",
+		},
+		{
+			name: "easter",
+			display_name: "Easter",
+		},
+		{
+			name: "valentines_day",
+			display_name: "Valentine's Day",
+		},
+		{
+			name: "halloween",
+			display_name: "Halloween",
+		},
+		{
+			name: "new_years",
+			display_name: "New Years",
+		},
+	],
+};
+
+export default categories;
